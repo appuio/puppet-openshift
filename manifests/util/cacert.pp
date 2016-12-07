5d77a4ae565c0ea6d1fb6f69d8fbacd617f69d04
Michael Hanselmann <hansmi@vshn.ch>
2016-12-07 15:39:23 +0100

    Add DigiCert SHA2 High Assurance Server CA

--- manifests/util/cacert.pp
+++ manifests/util/cacert.pp
@@ -11,17 +11,23 @@ class openshift::util::cacert {
 
   $_anchor_dir = '/etc/pki/ca-trust/source/anchors'
 
-  # As of December 7, 2016 "registry.access.redhat.com" uses a certificate not
-  # included in the certificate authorities provided by the base system
-  file { "${_anchor_dir}/DigiCertSHA2ExtendedValidationServerCA.pem":
+  # As of December 7, 2016 "registry.access.redhat.com" is signed by
+  # certificate authorities not included with the base system
+  create_resources('file', {
+    "${_anchor_dir}/DigiCertSHA2ExtendedValidationServerCA.pem" => {
+      source => "puppet:///modules/${module_name}/DigiCertSHA2ExtendedValidationServerCA.pem",
+    },
+    "${_anchor_dir}/DigiCertSHA2HighAssuranceServerCA.pem" => {
+      source => "puppet:///modules/${module_name}/DigiCertSHA2HighAssuranceServerCA.pem",
+    },
+  }, {
     ensure  => file,
     owner   => 'root',
     group   => 'root',
     mode    => '0644',
-    source  => "puppet:///modules/${module_name}/DigiCertSHA2ExtendedValidationServerCA.pem",
     require => Package['ca-certificates'],
     notify  => Exec['openshift-update-ca-trust'],
-  }
+  })
 
   Package['ca-certificates'] ->
   exec { 'openshift-update-ca-trust':
