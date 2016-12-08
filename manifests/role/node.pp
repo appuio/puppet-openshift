4e03124cf5d2a8c947aa15990f18bc4c3c34322d
Michael Hanselmann <hansmi@vshn.ch>
2016-12-08 09:49:43 +0100

    Set correct name for NetworkManager service

--- manifests/role/node.pp
+++ manifests/role/node.pp
@@ -24,6 +24,7 @@ class openshift::role::node (
     #
     Package[NetworkManager] ->
     service { 'openshift-networkmanager':
+      name   => 'NetworkManager',
       ensure => 'running',
       enable => true,
     }
