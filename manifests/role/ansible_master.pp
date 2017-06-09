1587d41a6f30a89c71ce917331e277a88c2c431d
Michael Hanselmann <hansmi@vshn.ch>
2017-06-09 14:35:36 +0200

    Use "ini_setting" instead of Augeas with Puppet.lns
    "ansible.cfg" is an INI file. Use per-entry "ini_setting" resources
    to manage two entries.

--- manifests/role/ansible_master.pp
+++ manifests/role/ansible_master.pp
@@ -32,18 +32,24 @@ class openshift::role::ansible_master (
     provider => git,
     revision => $playbooks_version,
     source   => $playbooks_source,
-  } ->
+  }
 
-  # Set some Ansible configuration values
-  augeas { 'ansible.cfg':
-    lens    => 'Puppet.lns',
-    incl    => '/etc/ansible/ansible.cfg',
-    changes => [
-      'set /files/etc/ansible/ansible.cfg/ssh_connection/pipelining True',
-      'set /files/etc/ansible/ansible.cfg/ssh_connection/control_path /tmp/ansible-ssh-%%h-%%p-%%r',
-    ],
-    require => Package['ansible'],
-  } ->
+  create_resources('ini_setting', prefix({
+    'ssh-conn-pipelining' => {
+      section => 'ssh_connection',
+      setting => 'pipelining',
+      value   => 'True',
+    },
+    'ssh-conn-controlpath' => {
+      section => 'ssh_connection',
+      setting => 'control_path',
+      value   => '/tmp/ansible-ssh-%%C',
+    },
+    }, 'ansible-cfg-'), {
+      ensure  => present,
+      path    => '/etc/ansible/ansible.cfg',
+      require => Package[ansible],
+    })
 
   # Write Ansible hosts file (this is the main configuration!)
   file { '/etc/ansible/hosts':
